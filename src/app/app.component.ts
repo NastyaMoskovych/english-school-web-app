5a29539a97c81e677bcea01888fd7802007289eb
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2023-10-30 18:52:33 +0200

    Multi-language support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'english-school-web-app';
+  private languageKey = 'e-school:language';
+  public selectedLanguage = this.getDefaultLanguage();
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'ua']);
+    translate.setDefaultLang(this.getDefaultLanguage());
+  }
+
+  onChangeLanguage(language: string): void {
+    localStorage.setItem(this.languageKey, language);
+    this.selectedLanguage = language;
+    this.translate.use(language);
+  }
+
+  private getDefaultLanguage(): string {
+    const language = localStorage.getItem(this.languageKey);
+
+    if (language) {
+      return language;
+    }
+
+    return this.translate.getBrowserLang() || 'ua';
+  }
 }

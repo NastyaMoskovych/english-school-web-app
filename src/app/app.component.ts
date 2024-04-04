65b0b01911f766b926caf00373dd771354b376df
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2024-04-04 11:02:45 +0300

    PWA - version ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  SwUpdate,
+  VersionEvent,
+  VersionReadyEvent,
+} from '@angular/service-worker';
 import { TranslateService } from '@ngx-translate/core';
 import {
   FooterComponent,
@@ -9,7 +14,7 @@ import {
   getDefaultLanguage,
 } from '@shared/components';
 import { IUser } from '@shared/models';
-import { Observable } from 'rxjs';
+import { Observable, filter } from 'rxjs';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -29,9 +34,25 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   public authService = inject(AuthService);
   public user$: Observable<IUser | null> = this.authService.user$;
+  private swUpdate = inject(SwUpdate);
 
   constructor(private translate: TranslateService) {
     translate.addLangs(['en', 'uk']);
     translate.setDefaultLang(getDefaultLanguage(this.translate));
+
+    if (this.swUpdate.isEnabled) {
+      this.initUpdate();
+    }
+  }
+
+  private initUpdate(): void {
+    this.swUpdate.versionUpdates
+      .pipe(
+        filter(
+          (evt: VersionEvent): evt is VersionReadyEvent =>
+            evt.type === 'VERSION_READY',
+        ),
+      )
+      .subscribe(() => window.location.reload());
   }
 }
